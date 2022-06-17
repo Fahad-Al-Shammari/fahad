4156335cd0bb75727ae6ad4b94887ef9f6125873
Fahad Al-Shammari <102398370+Fahad-Al-Shammari@users.noreply.github.com>
2022-06-17 08:21:35 +0300

    Update advance.py

--- Zaid/Player/advance.py
+++ Zaid/Player/advance.py
@@ -31,11 +31,11 @@ async def alive(event):
                                   caption=DEADLY,
                                   buttons=[
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/Superior_Bots"),
-        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/Superior_Support")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/FF7F2"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/FF7F2")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ITZ-ZAID/Zaid-Vc-Player")
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/Fahad-Al-Shammari/fahad")
         ]
         ]
         )
